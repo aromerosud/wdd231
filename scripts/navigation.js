3aea7d44bc17f1cb926a9f118ea17694eaf715be
Arturo Romero <romerosud89@gmail.com>
2026-01-08 18:27:46 -0500

    fix navigation and multiple colors

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -10,9 +10,10 @@ tempButton.addEventListener("click", () => {
 const navLinks = document.querySelectorAll(".navigation a");
 
 navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.forEach(l => l.classList.remove("active"));
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
 
+        navLinks.forEach(l => l.classList.remove("active"));
         link.classList.add("active");
     });
 });
