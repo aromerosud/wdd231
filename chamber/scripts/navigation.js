a36b74cdfd034c8232cc197086cf8793e5f3919e
Arturo Romero <romerosud89@gmail.com>
2026-01-15 18:57:23 -0500

    fix menu header

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,9 +1,22 @@
 const tempButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
+const titleSpan = document.querySelector("#chamber-title");
+const logo = document.querySelector("#logo-header");
+const icon = document.querySelector("#icon-header");
 
 tempButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     tempButton.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        titleSpan.style.display = "none";
+        logo.style.display = "none";
+        icon.style.display = "none";
+    } else {
+        titleSpan.style.display = "flex";
+        logo.style.display = "flex";
+        icon.style.display = "flex";
+    }
 });
 
 const navLinks = document.querySelectorAll(".navigation a");
